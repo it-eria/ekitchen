c17b1477ded131d8a665c4bccfae1d9534551d40
it-eria <iteria.info@gmail.com>
2018-05-04 13:27:05 +0300

    add newform review and new page news single

--- src/js/script.js
+++ src/js/script.js
@@ -462,3 +462,84 @@ $('.single-portfolio-slider-container__width-1440__content__slider-portfolio-thu
     arrows: false
 
 });
+
+
+
+$(document).ready(function(){
+    $('.form-element').focus(function(){
+        $(this).siblings('label').addClass('focus');
+    });
+    $('.form-element').focusout(function(){
+        if ($(this).val() === ''){
+            $(this).siblings('label').removeClass('focus');
+        }
+    });
+});
+
+$(function() {
+    function maskImgs() {
+        //$('.img-wrapper img').imagesLoaded({}, function() {
+        $.each($('.img-wrapper img'), function(index, img) {
+            var src = $(img).attr('src');
+            var parent = $(img).parent();
+            parent
+                .css('background', 'url(' + src + ') no-repeat center center')
+                .css('background-size', 'cover');
+            $(img).remove();
+        });
+        //});
+    }
+
+    var preview = {
+        init: function() {
+            preview.setPreviewImg();
+            preview.listenInput();
+        },
+        setPreviewImg: function(fileInput) {
+            var path = $(fileInput).val();
+            var uploadText = $(fileInput).siblings('.file-upload-text');
+
+            if (!path) {
+                $(uploadText).val('');
+            } else {
+                path = path.replace(/^C:\\fakepath\\/, "");
+                $(uploadText).val(path);
+
+                preview.showPreview(fileInput, path, uploadText);
+            }
+        },
+        showPreview: function(fileInput, path, uploadText) {
+            var file = $(fileInput)[0].files;
+
+            if (file && file[0]) {
+                var reader = new FileReader();
+
+                reader.onload = function(e) {
+                    var previewImg = $(fileInput).parents('.file-upload-wrapper').siblings('.preview');
+                    var img = $(previewImg).find('img');
+
+                    if (img.length == 0) {
+                        $(previewImg).html('<img src="' + e.target.result + '" alt=""/>');
+                    } else {
+                        img.attr('src', e.target.result);
+                    }
+
+                    uploadText.val(path);
+                    maskImgs();
+                }
+
+                reader.onloadstart = function() {
+                    $(uploadText).val('uploading..');
+                }
+
+                reader.readAsDataURL(file[0]);
+            }
+        },
+        listenInput: function() {
+            $('.file-upload-native').on('change', function() {
+                preview.setPreviewImg(this);
+            });
+        }
+    };
+    preview.init();
+});
